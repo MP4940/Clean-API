cab79a8018b5f1360a0108f78e292ff9d5d68f50
MP4940 <maxpersson40@gmail.com>
2023-11-22 15:05:48 +0100

    Tog bort test för Bark metoden, då det inte är med i kravlistan

--- Test/DogTest/DogTest.cs
+++ Test/DogTest/DogTest.cs
@@ -6,32 +6,13 @@ namespace Test.DogTest
     [TestFixture]
     public class DogTest
     {
-        //private readonly MockDatabase _mockDatabase;
-        [TestCase("Bark")]
-        public void TestMethodBark(string expected)
-        {
-            // Act
-            Dog dog = new Dog();
-            string actual = dog.Bark();
-
-            // Assert
-            Assert.That(expected, Is.EqualTo(actual));
-        }
-        // Ta in från mockDB listan
         [TestCase("Stanley")]
         public void TestMockDatabaseDogName(string expected)
         {
-            // Testa mockDatabase dog name mot API
             MockDatabase _mockDatabase = new MockDatabase();
-
-            // Act
-            // få in svar från mockDB
             string actual = _mockDatabase.allDogs.FirstOrDefault().Name;
 
-            // Assert
             Assert.That(expected, Is.EqualTo(actual));
         }
-
-        // Testa mockDatabase dog Guid mot APi
     }
 }
