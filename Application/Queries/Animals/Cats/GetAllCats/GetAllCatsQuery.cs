aa053674de43a27d456f0e4b76d91ef03a568768
MP4940 <maxpersson40@gmail.com>
2023-11-27 14:03:37 +0100

    Skapade test för allCats med tillhörande controller, query, query handler, model, infrastructure.

---
+++ Application/Queries/Animals/Cats/GetAllCats/GetAllCatsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models.Animals.Cats;
+using MediatR;
+
+namespace Application.Queries.Animals.Cats.GetAllCats
+{
+    public class GetAllCatsQuery : IRequest<List<Cat>>
+    {
+    }
+}
---
+++ Test/AnimalTests/CatTest/QueryTest/GetAllCatsTests.cs
@@ -0,0 +1,34 @@
+﻿using Application.Queries.Animals.Cats.GetAllCats;
+using Infrastructure.Database;
+
+namespace Test.AnimalTests.CatTest.QueryTest
+{
+    [TestFixture]
+    internal class GetAllCatsTests
+    {
+        private MockDatabase _mockDatabase;
+        private GetAllCatsQueryHandler _GetAllCatsQueryHandler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDatabase = new MockDatabase();
+            _GetAllCatsQueryHandler = new GetAllCatsQueryHandler(_mockDatabase);
+        }
+
+        [Test]
+        public async Task Is_Not_Empty_And_Returns_AllCats_Correctly()
+        {
+            // Arrange
+            var allCats = _mockDatabase.allCats;
+            var query = new GetAllCatsQuery();
+
+            // Act
+            var result = await _GetAllCatsQueryHandler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.That(allCats.Count > 0);
+            Assert.That(result, Is.EqualTo(allCats));
+        }
+    }
+}
