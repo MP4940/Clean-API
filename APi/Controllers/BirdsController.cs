260dfdc9e1a83524acb91ae403eddb19cf97995b
MP4940 <maxpersson40@gmail.com>
2023-12-11 09:16:11 +0100

    Felhantering i APi samt validering

--- APi/Controllers/BirdsController.cs
+++ APi/Controllers/BirdsController.cs
@@ -4,6 +4,8 @@ using Application.Commands.Animals.Birds.UpdateBird;
 using Application.Dtos.AnimalsDtos.BirdDto;
 using Application.Queries.Animals.Birds.GetAllBirds;
 using Application.Queries.Animals.Birds.GetBirdByID;
+using Application.Validators;
+using Application.Validators.Bird;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,23 +17,50 @@ namespace APi.Controllers
     public class BirdsController : ControllerBase
     {
         internal readonly IMediator _mediatR;
-        public BirdsController(IMediator mediatR)
+        internal readonly GuidValidator _guidValidator;
+        internal readonly BirdValidator _birdValidator;
+
+        public BirdsController(IMediator mediatR, GuidValidator guidValidator, BirdValidator birdValidator)
         {
             _mediatR = mediatR;
+            _guidValidator = guidValidator;
+            _birdValidator = birdValidator;
         }
 
         [HttpGet]
         [Route("getAllBirds")]
         public async Task<IActionResult> GetAllBirds()
         {
-            return Ok(await _mediatR.Send(new GetAllBirdsQuery()));
+            try
+            {
+                return Ok(await _mediatR.Send(new GetAllBirdsQuery()));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         [HttpGet]
         [Route("getBirdById/{birdID}")]
         public async Task<IActionResult> GetBirdById(Guid birdID)
         {
-            return Ok(await _mediatR.Send(new GetBirdByIDQuery(birdID)));
+            var guidToValidate = _guidValidator.Validate(birdID);
+
+            // Error handling
+            if (!guidToValidate.IsValid)
+            {
+                return BadRequest(guidToValidate.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            try
+            {
+                return Ok(await _mediatR.Send(new GetBirdByIDQuery(birdID)));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         // Create a new Bird 
@@ -40,7 +69,24 @@ namespace APi.Controllers
         [Route("addNewBird")]
         public async Task<IActionResult> AddBird([FromBody] BirdDto newBird)
         {
-            return Ok(await _mediatR.Send(new AddBirdCommand(newBird)));
+            // Validate Bird
+            var birdToValidate = _birdValidator.Validate(newBird);
+
+            // Error handling
+            if (!birdToValidate.IsValid)
+            {
+                return BadRequest(birdToValidate.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            // Try catch
+            try
+            {
+                return Ok(await _mediatR.Send(new AddBirdCommand(newBird)));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         // Update a specific Bird
@@ -49,7 +95,24 @@ namespace APi.Controllers
         [Route("updateBird/{updatedBirdID}")]
         public async Task<IActionResult> UpdateBird([FromBody] BirdDto updatedBird, Guid updatedBirdID)
         {
-            return Ok(await _mediatR.Send(new UpdateBirdByIDCommand(updatedBird, updatedBirdID)));
+            // Validate Bird
+            var birdToValidate = _birdValidator.Validate(updatedBird);
+
+            // Error handling
+            if (!birdToValidate.IsValid)
+            {
+                return BadRequest(birdToValidate.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            // Try catch
+            try
+            {
+                return Ok(await _mediatR.Send(new UpdateBirdByIDCommand(updatedBird, updatedBirdID)));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         // Delete a specific Bird
@@ -58,7 +121,22 @@ namespace APi.Controllers
         [Route("deleteBird/{deletedBirdID}")]
         public async Task<IActionResult> DeleteBird(Guid deletedBirdID)
         {
-            return Ok(await _mediatR.Send(new DeleteBirdByIDCommand(deletedBirdID)));
+            var guidToValidate = _guidValidator.Validate(deletedBirdID);
+
+            // Error handling
+            if (!guidToValidate.IsValid)
+            {
+                return BadRequest(guidToValidate.Errors.ConvertAll(errors => errors.ErrorMessage));
+            }
+
+            try
+            {
+                return Ok(await _mediatR.Send(new DeleteBirdByIDCommand(deletedBirdID)));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
---
+++ Application/Validators/Dog/DogValidator.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos.AnimalsDtos.DogDto;
+using FluentValidation;
+
+namespace Application.Validators.Dog
+{
+    public class DogValidator : AbstractValidator<DogDto>
+    {
+        public DogValidator()
+        {
+            RuleFor(dog => dog.Name)
+                .NotEmpty().WithMessage("Dog name cannot be empty")
+                .NotNull().WithMessage("Dog name cannot be null");
+        }
+    }
+}
