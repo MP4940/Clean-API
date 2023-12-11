ce76c6b2e7f0451ac7f35f2be67e558352932971
MP4940 <maxpersson40@gmail.com>
2023-12-11 09:17:23 +0100

    Formatering

--- APi/Controllers/DogsController.cs
+++ APi/Controllers/DogsController.cs
@@ -10,8 +10,6 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace APi.Controllers
 {
     [Route("api/[controller]")]
