fcb67add9e382a5bf331f1db14f0dc3304b92142
MP4940 <maxpersson40@gmail.com>
2023-12-25 11:29:20 +0100

    Fixade test för GetAllUsers

--- Test/Repository/UserTests/GetAllUsers/GetAllUsersTests.cs
+++ Test/Repository/UserTests/GetAllUsers/GetAllUsersTests.cs
@@ -5,34 +5,41 @@ using Moq;
 
 namespace Test.Repository.UserTests.GetAllUsers
 {
-    //[TestFixture]
-    //internal class GetAllUsersTests
-    //{
-    //    private Mock<IUserRepository> _userRepositoryMock;
-    //    private GetAllUsersQueryHandler _handler;
+    [TestFixture]
+    internal class GetAllUsersTests
+    { 
+        private Mock<IUserRepository> _userRepositoryMock;
+        private GetAllUsersQueryHandler _getAllUsersQueryHandler;
 
-    //    [SetUp]
-    //    public void SetUp()
-    //    {
-    //        _userRepositoryMock = new Mock<IUserRepository>();
-    //        _handler = new GetAllUsersQueryHandler(_userRepositoryMock.Object);
-    //    }
+        [SetUp]
+        public void SetUp()
+        {
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _getAllUsersQueryHandler = new GetAllUsersQueryHandler(_userRepositoryMock.Object);
+        }
 
-    //    [Test]
-    //    public async Task GetAllUsers_returns_correct()
-    //    {
-    //        // Arrange
-    //        var test = _userRepositoryMock.Object.GetAllUsers();
+        [Test]
+        public async Task GetAlLDogs_Returns_Correct()
+        {
+            // Arrange
+            List<User> users =
+            [
+                new User() { ID = new Guid("12345678-1234-5678-1234-567812345674"), Username = "testUser2", Password = "password", Authorized = true, Role = "admin" },
+                new User() { ID = new Guid("34d621a5-9f60-4647-bcb7-adcfdbd8dbdb"), Username = "admin1", Password = "Password123!", Authorized = true, Role = "admin" },
+            ];
 
-    //        var query = new GetAllUsersQuery();
 
-    //        // Act
-    //        var result = await _handler.Handle(query, CancellationToken.None);
+            _userRepositoryMock.Setup(x => x.GetAllUsers()).ReturnsAsync(users);
 
-    //        // Assert
-    //        Assert.That(result, Is.Not.Null);
-    //        Assert.That(result, Is.InstanceOf<List<User>>());
-    //        Assert.That(result, Is.Not.Empty);
-    //    }
-    //}
+            var query = new GetAllUsersQuery();
+
+            // Act
+            var result = await _getAllUsersQueryHandler.Handle(query, CancellationToken.None);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<List<User>>());
+            Assert.That(result, Is.Not.Empty);
+        }
+    }
 }
---
+++ Test/Repository/UserTests/UpdateUser/UpdateUserTests.cs
@@ -0,0 +1,10 @@
+﻿using Application.Commands.Users.Update;
+using Application.Dtos.UserDtos;
+using Domain.Models.Users;
+using Infrastructure.Repositories.Users;
+using Moq;
+
+namespace Test.Repository.UserTests.UpdateUser
+{
+    
+}
