739b7564524ff1ed2261007153dda3ef19f21420
MP4940 <maxpersson40@gmail.com>
2024-02-06 15:55:06 +0100

    Added logging to animal repositories

--- Infrastructure/Repositories/Animals/Birds/BirdRepository.cs
+++ Infrastructure/Repositories/Animals/Birds/BirdRepository.cs
@@ -1,26 +1,33 @@
 ﻿using Domain.Models.Animals.Birds;
 using Infrastructure.Database;
+using Serilog;
 
 namespace Infrastructure.Repositories.Animals.Birds
 {
     public class BirdRepository : IBirdRepository
     {
         private readonly RealDatabase _realDatabase;
+        private readonly ILogger _logger;
+
 
         public BirdRepository(RealDatabase realDatabase)
         {
             _realDatabase = realDatabase;
+            _logger = Log.ForContext<BirdRepository>();
         }
 
         public async Task<List<Bird>> GetAllBirds()
         {
             try
             {
+                _logger.Information("Getting all birds...");
                 List<Bird> allBirdsFromDatabase = _realDatabase.Birds.ToList();
+                _logger.Information($"Found {allBirdsFromDatabase.Count} birds.");
                 return await Task.FromResult(allBirdsFromDatabase);
             }
             catch (ArgumentException e)
             {
+                _logger.Error(e, "An error occurred while getting all birds.");
                 throw new ArgumentException(e.Message);
             }
         }
@@ -29,13 +36,14 @@ namespace Infrastructure.Repositories.Animals.Birds
         {
             try
             {
+                _logger.Information($"Getting bird by ID {id}...");
                 var wantedBird = _realDatabase.Birds.Where(bird => bird.AnimalID == id).FirstOrDefault()!;
+                _logger.Information($"Bird with ID {wantedBird.AnimalID} found.");
                 return await Task.FromResult(wantedBird);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering bird");
+                _logger.Error(e, $"An error occurred while getting bird by ID {id}.");
                 throw new ArgumentException(e.Message);
             }
         }
@@ -44,14 +52,15 @@ namespace Infrastructure.Repositories.Animals.Birds
         {
             try
             {
+                _logger.Information($"Adding new bird...");
                 _realDatabase.Birds.Add(birdToAdd);
                 _realDatabase.SaveChanges();
+                _logger.Information($"Bird added successfully.");
                 return await Task.FromResult(birdToAdd);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering user");
+                _logger.Error(e, $"Error adding bird.");
                 throw new ArgumentException(e.Message);
             }
         }
@@ -60,14 +69,15 @@ namespace Infrastructure.Repositories.Animals.Birds
         {
             try
             {
+                _logger.Information($"Updating bird with ID {birdToUpdate.AnimalID}...");
                 _realDatabase.Birds.Update(birdToUpdate);
                 _realDatabase.SaveChanges();
+                _logger.Information($"Bird with ID {birdToUpdate.AnimalID} updated successfully.");
                 return await Task.FromResult(birdToUpdate);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering user");
+                _logger.Error(e, $"An error occurred while updating bird with ID {birdToUpdate.AnimalID}.");
                 throw new ArgumentException(e.Message);
             }
         }
@@ -76,14 +86,15 @@ namespace Infrastructure.Repositories.Animals.Birds
         {
             try
             {
+                _logger.Information($"Deleting bird with ID {birdToDelete.AnimalID}...");
                 _realDatabase.Birds.Remove(birdToDelete);
                 _realDatabase.SaveChanges();
+                _logger.Information($"Bird with ID {birdToDelete.AnimalID} deleted successfully.");
                 return await Task.FromResult(birdToDelete);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering user");
+                _logger.Error(e, $"An error occurred while deleting bird with ID {birdToDelete.AnimalID}.");
                 throw new ArgumentException(e.Message);
             }
         }
--- Infrastructure/Repositories/Animals/Cats/CatRepository.cs
+++ Infrastructure/Repositories/Animals/Cats/CatRepository.cs
@@ -1,26 +1,33 @@
 ﻿using Domain.Models.Animals.Cats;
 using Infrastructure.Database;
+using Serilog;
 
 namespace Infrastructure.Repositories.Animals.Cats
 {
     public class CatRepository : ICatRepository
     {
         private readonly RealDatabase _realDatabase;
+        private readonly ILogger _logger;
+
 
         public CatRepository(RealDatabase realDatabase)
         {
             _realDatabase = realDatabase;
+            _logger = Log.ForContext<CatRepository>();
         }
 
         public async Task<List<Cat>> GetAllCats()
         {
             try
             {
+                _logger.Information("Getting all cats...");
                 List<Cat> allCatsFromDatabase = _realDatabase.Cats.ToList();
+                _logger.Information($"Found {allCatsFromDatabase.Count} cats.");
                 return await Task.FromResult(allCatsFromDatabase);
             }
             catch (ArgumentException e)
             {
+                _logger.Error(e, "An error occurred while getting all cats.");
                 throw new ArgumentException(e.Message);
             }
         }
@@ -29,61 +36,65 @@ namespace Infrastructure.Repositories.Animals.Cats
         {
             try
             {
-                var wantedCat = _realDatabase.Cats.Where(Cat => Cat.AnimalID == id).FirstOrDefault()!;
+                _logger.Information($"Getting cat by ID {id}...");
+                var wantedCat = _realDatabase.Cats.Where(cat => cat.AnimalID == id).FirstOrDefault()!;
+                _logger.Information($"cat with ID {wantedCat.AnimalID} found.");
                 return await Task.FromResult(wantedCat);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering Cat");
+                _logger.Error(e, $"An error occurred while getting cat by ID {id}.");
                 throw new ArgumentException(e.Message);
             }
         }
 
-        public async Task<Cat> AddCat(Cat CatToAdd)
+        public async Task<Cat> AddCat(Cat catToAdd)
         {
             try
             {
-                _realDatabase.Cats.Add(CatToAdd);
+                _logger.Information($"Adding new cat...");
+                _realDatabase.Cats.Add(catToAdd);
                 _realDatabase.SaveChanges();
-                return await Task.FromResult(CatToAdd);
+                _logger.Information($"cat added successfully.");
+                return await Task.FromResult(catToAdd);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering user");
+                _logger.Error(e, $"Error adding cat.");
                 throw new ArgumentException(e.Message);
             }
         }
 
-        public async Task<Cat> UpdateCat(Cat CatToUpdate)
+        public async Task<Cat> UpdateCat(Cat catToUpdate)
         {
             try
             {
-                _realDatabase.Cats.Update(CatToUpdate);
+                _logger.Information($"Updating cat with ID {catToUpdate.AnimalID}...");
+                _realDatabase.Cats.Update(catToUpdate);
                 _realDatabase.SaveChanges();
-                return await Task.FromResult(CatToUpdate);
+                _logger.Information($"Cat with ID {catToUpdate.AnimalID} updated successfully.");
+                return await Task.FromResult(catToUpdate);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering user");
+                _logger.Error(e, $"An error occurred while updating cat with ID {catToUpdate.AnimalID}.");
                 throw new ArgumentException(e.Message);
             }
         }
 
-        public async Task<Cat> DeleteCat(Cat CatToDelete)
+        public async Task<Cat> DeleteCat(Cat catToDelete)
         {
             try
             {
-                _realDatabase.Cats.Remove(CatToDelete);
+                _logger.Information($"Deleting cat with ID {catToDelete.AnimalID}...");
+                _realDatabase.Cats.Remove(catToDelete);
                 _realDatabase.SaveChanges();
-                return await Task.FromResult(CatToDelete);
+                _logger.Information($"Cat with ID {catToDelete.AnimalID} deleted successfully.");
+                return await Task.FromResult(catToDelete);
             }
             catch (ArgumentException e)
             {
-                //// Log the error and return an error response
-                //_logger.LogError(e, "Error registering user");
+                _logger.Error(e, $"An error occurred while deleting cat with ID {catToDelete.AnimalID}.");
                 throw new ArgumentException(e.Message);
             }
         }
