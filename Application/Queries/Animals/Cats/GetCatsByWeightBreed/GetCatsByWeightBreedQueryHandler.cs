3f173f8b278a945b9ab1079bbcff09d70981cd25
MP4940 <maxpersson40@gmail.com>
2024-02-07 13:45:37 +0100

    Added getByWeightAndBreed to cat

---
+++ Application/Queries/Animals/Cats/GetCatsByWeightBreed/GetCatsByWeightBreedQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models.Animals.Cats;
+using Infrastructure.Repositories.Animals.Cats;
+using MediatR;
+
+namespace Application.Queries.Animals.Cats.GetCatsByWeightBreed
+{
+    public class GetCatsByWeightBreedQueryHandler : IRequestHandler<GetCatsByWeightBreedQuery, List<Cat>>
+    {
+        private readonly ICatRepository _catRepository;
+        public GetCatsByWeightBreedQueryHandler(ICatRepository catRepository)
+        {
+            _catRepository = catRepository;
+        }
+
+        public async Task<List<Cat>> Handle(GetCatsByWeightBreedQuery request, CancellationToken cancellationToken)
+        {
+            List<Cat> catsByWeightBreed = await _catRepository.GetCatsByWeightBreed(request.Breed, request.Weight);
+            return catsByWeightBreed;
+        }
+    }
+}
